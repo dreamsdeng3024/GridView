1d05921b4879c4fdc485c40b476338d896c4cac4
dreamsdeng <2998677551@qq.com>
2018-09-16 18:59:03 +0800

    分支修改，完成了游戏开发

--- src/com/kiritor/Test.java
+++ src/com/kiritor/Test.java
@@ -89,6 +89,20 @@ public class Test {
 		System.out.println("10");
 		System.out.println("11");
 		System.out.println("12");
+		
+		
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
+		System.out.println("hot--fix1");
 	}
 
 	
