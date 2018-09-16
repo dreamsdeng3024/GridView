e16971ddd5f1b78dbb9db72f7972c28c69f1b842
dreamsdeng <2998677551@qq.com>
2018-09-16 17:39:59 +0800

    提交内容到远程库

--- src/com/kiritor/Test.java
+++ src/com/kiritor/Test.java
@@ -32,6 +32,23 @@ public class Test {
 		System.out.println("Test");
 		System.out.println("Test");
 		System.out.println("TestTestTest");
+		
+		
+		System.out.println("第六次提交内容");
+		System.out.println("第六次提交内容");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("TestTestTest");
+		
+		
+		
+		System.out.println("第六次提交内容");
+		System.out.println("第六次提交内容");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("TestTestTest");
 	}
 
 	
