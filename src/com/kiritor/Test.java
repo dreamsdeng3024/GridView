a00bb34ae6b6a3d018088728fb5b37724c60eb09
dreamsdeng <2998677551@qq.com>
2018-09-16 18:16:33 +0800

    改了第一行

--- src/com/kiritor/Test.java
+++ src/com/kiritor/Test.java
@@ -3,7 +3,7 @@ package com.kiritor;
 public class Test {
 	
 	public static void show_message_ok(){
-		System.out.println("开始showMessage------->ok");
+		System.out.println("GridView------->ok");
 		
 		System.out.println("第二次编辑内容");
 		
